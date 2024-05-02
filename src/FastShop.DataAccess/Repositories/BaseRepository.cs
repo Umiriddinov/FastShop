46f8b0d42be9ae31586c534f743bb7d592fd6ee2
Temurbek <umiriddinovt@gmail.com>
2024-05-03 03:02:20 +0500

    update DataAccess

---
+++ src/FastShop.DataAccess/Repositories/BaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using Npgsql;
+
+namespace FastShop.DataAccess.Repositories;
+
+public class BaseRepository
+{
+    protected readonly NpgsqlConnection _connection;
+
+    public BaseRepository()
+    {
+        this._connection = new NpgsqlConnection("Host = localhost; Port = 5432; Database = fast-shop-db; User Id = postgres; Password = @20112606;");
+    }
+}
